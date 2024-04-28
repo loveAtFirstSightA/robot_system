b2e39d997697366854db9892fca55c7f6c905238
lio <lioSgr1205@gmail.com>
2024-04-28 18:07:47 +0800

    add: create package of robot_path_creater

---
+++ src/robot_path_creater/src/path_creater.cpp
@@ -0,0 +1,35 @@
+/*
+ Copyright 2024 ROS2 LLC
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+      https://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+
+#include "robot_path_creater/path_creater.hpp"
+
+namespace path_creater
+{
+
+PathCreater::PathCreater()
+: Node("robot_path_creater")
+{
+
+}
+
+PathCreater::~PathCreater()
+{
+     
+}
+
+
+}  // namespace path_creater
