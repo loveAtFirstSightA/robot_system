aebffe7d68cdfc228b0d8c7f6571ff7a135a42f7
lio <lioSgr1205@gmail.com>
2024-04-28 16:45:51 +0800

    add: facotry map and launch shell script

---
+++ rviz2.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+
+ros2 launch bcr_bot rviz.launch.py
---
+++ simulation.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+ros2 launch bcr_bot gazebo.launch.py
+
+# ros2 launch bcr_bot gazebo.launch.py \
+# 	camera_enabled:=True \
+# 	two_d_lidar_enabled:=True \
+# 	stereo_camera_enabled:=False \
+# 	position_x:=0.0 \
+# 	position_y:=0.0 \
+# 	orientation_yaw:=0.0 \
+# 	odometry_source:=world \
+# 	world_file:=small_warehouse.sdf \
+# 	robot_namespace:="bcr_bot"
