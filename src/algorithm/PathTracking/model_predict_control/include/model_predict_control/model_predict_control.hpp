c5b329485ebb783b311597e53df710fcc4ae78a5
lio <lioSgr1205@gmail.com>
2024-07-03 18:41:04 +0800

    create mpc package

---
+++ src/algorithm/PathTracking/model_predict_control/include/model_predict_control/model_predict_control.hpp
@@ -0,0 +1,37 @@
+/*
+ Copyright 2024 ROS2 LLC
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+      https://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+#ifndef MODEL_PREDICT_CONTROL__MODEL_PREDICT_CONTROL_HPP_
+#define MODEL_PREDICT_CONTROL__MODEL_PREDICT_CONTROL_HPP_
+
+#include "rclcpp/rclcpp.hpp"
+
+namespace model_predict_control
+{
+class ModelPredictControl : public rclcpp::Node
+{
+public:
+    ModelPredictControl();
+    ~ModelPredictControl();
+
+private:
+
+
+
+
+};
+}  // namespace model_predict_control
+#endif  // MODEL_PREDICT_CONTROL__MODEL_PREDICT_CONTROL_HPP_
