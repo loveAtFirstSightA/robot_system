643e2519e3f53c91cbb9617fdcba5f5c56b5065d
lio <lioSgr1205@gmail.com>
2024-09-09 18:49:59 +0800

    update date recorder

--- src/tools/data_recorder/include/data_recorder/data_recorder.hpp
+++ src/tools/data_recorder/include/data_recorder/data_recorder.hpp
@@ -20,7 +20,7 @@
 #include "rclcpp/rclcpp.hpp"
 #include "nav_msgs/msg/odometry.hpp"
 #include "geometry_msgs/msg/twist.hpp"
-#include "geometry_msgs/msg/vector3_stamped.hpp"
+#include "geometry_msgs/msg/vector3.hpp"
 #include "sensor_msgs/msg/imu.hpp"
 #include "tf2_ros/buffer.h"
 #include "tf2_ros/transform_listener.h"
@@ -37,25 +37,27 @@ public:
     ~DataRecorder();
 
 private:
-    void timer_callback();
+    bool getCurrentPose(geometry_msgs::msg::Vector3 & pose);
+    // void timer_callback();
     void odom_sub_callback(const nav_msgs::msg::Odometry::SharedPtr msg);
-    void odom_data_sub_callback(const nav_msgs::msg::Odometry::SharedPtr msg);
-    void imu_sub_callback(const sensor_msgs::msg::Imu::SharedPtr msg);
-    void imu_data_sub_callback(const sensor_msgs::msg::Imu::SharedPtr msg);
+    // void odom_data_sub_callback(const nav_msgs::msg::Odometry::SharedPtr msg);
+    // void imu_sub_callback(const sensor_msgs::msg::Imu::SharedPtr msg);
+    // void imu_data_sub_callback(const sensor_msgs::msg::Imu::SharedPtr msg);
     void vel_sub_callback(const geometry_msgs::msg::Twist::SharedPtr msg);
     
     rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
     nav_msgs::msg::Odometry odom_pose_;
-    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_data_sub_;
-    nav_msgs::msg::Odometry odom_data_pose_;
-    rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr imu_sub_;
-    sensor_msgs::msg::Imu imu_;
-    rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr imu_data_sub_;
-    sensor_msgs::msg::Imu imu_data_;
+    // rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_data_sub_;
+    // nav_msgs::msg::Odometry odom_data_pose_;
+    // rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr imu_sub_;
+    // sensor_msgs::msg::Imu imu_;
+    // rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr imu_data_sub_;
+    // sensor_msgs::msg::Imu imu_data_;
     rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr vel_sub_;
     geometry_msgs::msg::Twist vel_;
 
-    rclcpp::TimerBase::SharedPtr timer_;
+    // rclcpp::TimerBase::SharedPtr timer_;
+    geometry_msgs::msg::Vector3 pose_;
 
 private:
     std::shared_ptr<tf2_ros::Buffer> tf_buffer_;
@@ -63,7 +65,6 @@ private:
     // std::shared_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
     geometry_msgs::msg::Vector3 tf_pose_;
 
-    bool localization_{false};
 };
 }  // namespace data_recorder
 #endif  // DATA_RECORDER__DATA_RECORDER_HPP_
--- src/tools/data_recorder/src/data_recorder.cpp
+++ src/tools/data_recorder/src/data_recorder.cpp
@@ -26,25 +26,25 @@ namespace data_recorder
 {
 DataRecorder::DataRecorder() : Node("data_recorder")
 {
-    timer_ = this->create_wall_timer(
-        std::chrono::milliseconds(15),
-        std::bind(&DataRecorder::timer_callback, this));
+    // timer_ = this->create_wall_timer(
+    //     std::chrono::milliseconds(15),
+    //     std::bind(&DataRecorder::timer_callback, this));
     odom_sub_ = this->create_subscription<nav_msgs::msg::Odometry>(
         "odom",
         100,
         std::bind(&DataRecorder::odom_sub_callback, this, std::placeholders::_1));
-    odom_data_sub_ = this->create_subscription<nav_msgs::msg::Odometry>(
-        "/odom_data",
-        100,
-        std::bind(&DataRecorder::odom_data_sub_callback, this, std::placeholders::_1));
-    imu_sub_ = this->create_subscription<sensor_msgs::msg::Imu>(
-        "/imu",
-        100,
-        std::bind(&DataRecorder::imu_sub_callback, this, std::placeholders::_1));
-    imu_data_sub_ = this->create_subscription<sensor_msgs::msg::Imu>(
-        "/imu_data",
-        100,
-        std::bind(&DataRecorder::imu_data_sub_callback, this, std::placeholders::_1));
+    // odom_data_sub_ = this->create_subscription<nav_msgs::msg::Odometry>(
+    //     "/odom_data",
+    //     100,
+    //     std::bind(&DataRecorder::odom_data_sub_callback, this, std::placeholders::_1));
+    // imu_sub_ = this->create_subscription<sensor_msgs::msg::Imu>(
+    //     "/imu",
+    //     100,
+    //     std::bind(&DataRecorder::imu_sub_callback, this, std::placeholders::_1));
+    // imu_data_sub_ = this->create_subscription<sensor_msgs::msg::Imu>(
+    //     "/imu_data",
+    //     100,
+    //     std::bind(&DataRecorder::imu_data_sub_callback, this, std::placeholders::_1));
     vel_sub_ = this->create_subscription<geometry_msgs::msg::Twist>(
         "cmd_vel",
         100,
@@ -56,92 +56,91 @@ DataRecorder::DataRecorder() : Node("data_recorder")
 
 DataRecorder::~DataRecorder() {}
 
-void DataRecorder::timer_callback()
+// void DataRecorder::timer_callback()
+// {
+//     std::string source_link = "base_footprint";
+//     std::string target_link = "map";
+//     geometry_msgs::msg::TransformStamped tf;
+//     try {
+//         tf = tf_buffer_->lookupTransform(target_link, source_link, tf2::TimePointZero);
+//         tf_pose_.x = tf.transform.translation.x;
+//         tf_pose_.y = tf.transform.translation.y;
+//         tf_pose_.z = tf2::getYaw(tf.transform.rotation);
+//         localization_ = true;
+//     } catch(const std::exception& e) {
+//         // spdlog::warn("Not TF transform");
+//         localization_ = false;
+//     }
+// }
+
+void DataRecorder::odom_sub_callback(const nav_msgs::msg::Odometry::SharedPtr msg)
+{
+    this->odom_pose_ = *msg;
+
+    geometry_msgs::msg::Vector3 pose;
+    if (getCurrentPose(pose)) {
+        spdlog::info("odom: [x {:.4f}, y {:.4f}, yaw {:.4f}] estimate: [x {:.4f}, y {:.4f}, yaw {:.4f}], vel: [v {:.4f}, w {:.4f}]",
+        odom_pose_.pose.pose.position.x, odom_pose_.pose.pose.position.y, tf2::getYaw(odom_pose_.pose.pose.orientation),
+        pose.x, pose.y, pose.z,
+        vel_.linear.x, vel_.angular.z);
+    }
+
+// #define VELOCITY_MODE true
+
+// #if !VELOCITY_MODE
+//     // simple
+//     spdlog::info("odom: [x {:.4f}, y {:.4f}, yaw {:.4f}] vel: [v {:.4f}, w {:.4f}] estimate: [x {:.4f}, y {:.4f}, yaw {:.4f}] encoder: [x {:.4f}, y {:.4f}, yaw {:.4f}]",
+//         odom_pose_.pose.pose.position.x, odom_pose_.pose.pose.position.y, tf2::getYaw(odom_pose_.pose.pose.orientation),
+//         vel_.linear.x, vel_.angular.z,
+//         tf_pose_.x, tf_pose_.y, tf_pose_.z,
+//         odom_data_pose_.pose.pose.position.x, odom_data_pose_.pose.pose.position.y, tf2::getYaw(odom_data_pose_.pose.pose.orientation));
+// #elif VELOCITY_MODE
+//     // info v = 0 w != 0
+//     if (vel_.linear.x == 0) {
+//         spdlog::info("odom: [x {:.4f}, y {:.4f}, yaw {:.4f}] vel: [v {:.4f}, w {:.4f}] estimate: [x {:.4f}, y {:.4f}, yaw {:.4f}] encoder: [x {:.4f}, y {:.4f}, yaw {:.4f}]",
+//             odom_pose_.pose.pose.position.x, odom_pose_.pose.pose.position.y, tf2::getYaw(odom_pose_.pose.pose.orientation),
+//             vel_.linear.x, vel_.angular.z,
+//             tf_pose_.x, tf_pose_.y, tf_pose_.z,
+//             odom_data_pose_.pose.pose.position.x, odom_data_pose_.pose.pose.position.y, tf2::getYaw(odom_data_pose_.pose.pose.orientation));
+//     }
+// #endif
+}
+
+// void DataRecorder::odom_data_sub_callback(const nav_msgs::msg::Odometry::SharedPtr msg)
+// {
+//     this->odom_data_pose_ = *msg;
+// }
+
+// void DataRecorder::imu_sub_callback(const sensor_msgs::msg::Imu::SharedPtr msg)
+// {
+//     this->imu_ = *msg;
+// }
+
+// void DataRecorder::imu_data_sub_callback(const sensor_msgs::msg::Imu::SharedPtr msg)
+// {
+//     this->imu_data_ = *msg;
+// }
+
+void DataRecorder::vel_sub_callback(const geometry_msgs::msg::Twist::SharedPtr msg)
+{
+    this->vel_ = *msg;
+}
+
+bool DataRecorder::getCurrentPose(geometry_msgs::msg::Vector3 & pose)
 {
     std::string source_link = "base_footprint";
     std::string target_link = "map";
     geometry_msgs::msg::TransformStamped tf;
     try {
         tf = tf_buffer_->lookupTransform(target_link, source_link, tf2::TimePointZero);
-        tf_pose_.x = tf.transform.translation.x;
-        tf_pose_.y = tf.transform.translation.y;
-        tf_pose_.z = tf2::getYaw(tf.transform.rotation);
-        localization_ = true;
+        pose.x = tf.transform.translation.x;
+        pose.y = tf.transform.translation.y;
+        pose.z = tf2::getYaw(tf.transform.rotation);
+        return true;
     } catch(const std::exception& e) {
         // spdlog::warn("Not TF transform");
-        localization_ = false;
+        return false;
     }
-}
-
-void DataRecorder::odom_sub_callback(const nav_msgs::msg::Odometry::SharedPtr msg)
-{
-    if (!localization_) {
-        return;
-    }
-    this->odom_pose_ = *msg;
-    // spdlog::info("odom: [x {:.4f}, y {:.4f}, yaw {:.4f}] vel: [v {:.4f}, w {:.4f}] estimate: [x {:.4f}, y {:.4f}, yaw {:.4f}] encoder: [x {:.4f}, y {:.4f}, yaw {:.4f}] imu: linear_acc [x {:.4f}, y {:.4f}, z {:.4f}] angular_acc [x {:.4f}, y {:.4f}, z {:.4f}] yaw [{:.4f}] imu_data: linear_acc [x {:.4f}, y {:.4f}, z {:.4f}] angular_acc [x {:.4f}, y {:.4f}, z {:.4f}] yaw [{:.4f}]",
-    //     odom_pose_.pose.pose.position.x, odom_pose_.pose.pose.position.y, tf2::getYaw(odom_pose_.pose.pose.orientation),
-    //     vel_.linear.x, vel_.angular.z,
-    //     tf_pose_.x, tf_pose_.y, tf_pose_.z,
-    //     odom_data_pose_.pose.pose.position.x, odom_data_pose_.pose.pose.position.y, tf2::getYaw(odom_data_pose_.pose.pose.orientation),
-    //     imu_.linear_acceleration.x, imu_.linear_acceleration.y, imu_.linear_acceleration.z,
-    //     imu_.angular_velocity.x, imu_.angular_velocity.y, imu_.angular_velocity.z,
-    //     tf2::getYaw(imu_.orientation),
-    //     imu_data_.linear_acceleration.x, imu_data_.linear_acceleration.y, imu_data_.linear_acceleration.z,
-    //     imu_data_.angular_velocity.x, imu_data_.angular_velocity.y, imu_data_.angular_velocity.z,
-    //     tf2::getYaw(imu_data_.orientation));
-
-#define VELOCITY_MODE true
-
-#if !VELOCITY_MODE
-    // simple
-    spdlog::info("odom: [x {:.4f}, y {:.4f}, yaw {:.4f}] vel: [v {:.4f}, w {:.4f}] estimate: [x {:.4f}, y {:.4f}, yaw {:.4f}] encoder: [x {:.4f}, y {:.4f}, yaw {:.4f}]",
-        odom_pose_.pose.pose.position.x, odom_pose_.pose.pose.position.y, tf2::getYaw(odom_pose_.pose.pose.orientation),
-        vel_.linear.x, vel_.angular.z,
-        tf_pose_.x, tf_pose_.y, tf_pose_.z,
-        odom_data_pose_.pose.pose.position.x, odom_data_pose_.pose.pose.position.y, tf2::getYaw(odom_data_pose_.pose.pose.orientation));
-#elif VELOCITY_MODE
-    // info v = 0 w != 0
-    if (vel_.linear.x == 0) {
-        spdlog::info("odom: [x {:.4f}, y {:.4f}, yaw {:.4f}] vel: [v {:.4f}, w {:.4f}] estimate: [x {:.4f}, y {:.4f}, yaw {:.4f}] encoder: [x {:.4f}, y {:.4f}, yaw {:.4f}]",
-            odom_pose_.pose.pose.position.x, odom_pose_.pose.pose.position.y, tf2::getYaw(odom_pose_.pose.pose.orientation),
-            vel_.linear.x, vel_.angular.z,
-            tf_pose_.x, tf_pose_.y, tf_pose_.z,
-            odom_data_pose_.pose.pose.position.x, odom_data_pose_.pose.pose.position.y, tf2::getYaw(odom_data_pose_.pose.pose.orientation));
-    }
-#endif
-}
-
-void DataRecorder::odom_data_sub_callback(const nav_msgs::msg::Odometry::SharedPtr msg)
-{
-    if (!localization_) {
-        return;
-    }
-    this->odom_data_pose_ = *msg;
-}
-
-void DataRecorder::imu_sub_callback(const sensor_msgs::msg::Imu::SharedPtr msg)
-{
-    if (!localization_) {
-        return;
-    }
-    this->imu_ = *msg;
-}
-
-void DataRecorder::imu_data_sub_callback(const sensor_msgs::msg::Imu::SharedPtr msg)
-{
-    if (!localization_) {
-        return;
-    }
-    this->imu_data_ = *msg;
-}
-
-void DataRecorder::vel_sub_callback(const geometry_msgs::msg::Twist::SharedPtr msg)
-{
-    if (!localization_) {
-        return;
-    }
-    this->vel_ = *msg;
 }
 
 }  // namespace data_recorder
