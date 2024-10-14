3dc2bf7db09778b52fedd7007fd17043da47a7e1
lio <lioSgr1205@gmail.com>
2024-10-14 17:01:17 +0800

    add \n

--- src/demo_unit/odometry_covariance/src/odometry_covariance.cpp
+++ src/demo_unit/odometry_covariance/src/odometry_covariance.cpp
@@ -77,6 +77,7 @@ void OdometryCovariance::odomSubCallback(const nav_msgs::msg::Odometry::SharedPt
     }
     twist_cov << "]";
     spdlog::info("{}", twist_cov.str());
+    spdlog::info("\n");
 }
 
 /*
